e13fb15d3703bbd31190e3b291ae0994095be1a5
Sergio <sergiomendia33@gmail.com>
2022-07-02 18:28:42 -0600

    Examen Completo con Sentry
    Implementation log de errors and info

--- WebAPI/Controllers/CalculoClienteController.cs
+++ WebAPI/Controllers/CalculoClienteController.cs
@@ -1,6 +1,9 @@
 ﻿using Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Models;
+using Sentry;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -17,35 +20,84 @@ namespace WebAPI.Controllers
     {
         private readonly ICalculosCliente _service;
         private readonly IDbConnection _dbConnection;
-        public CalculoClienteController(ICalculosCliente cliente, IDbConnection dbConnection)
+        private readonly ILogger<CalculoClienteController> _logger;
+        public CalculoClienteController(ICalculosCliente cliente, IDbConnection dbConnection, ILogger<CalculoClienteController> logger)
         {
             this._service = cliente;
             this._dbConnection = dbConnection;
+            _logger = logger;
         }
         // GET: api/<CalculoClienteController>
         [HttpGet]
         public List<InfoCliente> Get()
         {
-            return _service.GetCliente(_dbConnection);
-        }
+            try
+            {
+                SentrySdk.CaptureMessage("Get Complete ", SentryLevel.Info);
+                return _service.GetCliente(_dbConnection);
+               
+            }
+            catch (Exception ex)
+            {
 
+                SentrySdk.CaptureException(ex);
+                SentrySdk.CaptureMessage("Error en el metodo Get Info Cliente", SentryLevel.Error);
+                throw ex;
+            }
+            
+        }
+       
+        
         // POST api/<CalculoClienteController>
         [HttpPost("CalculoISR")]
         public List<Calculo_Cliente> Post(int id_cliente)
         {
-            return _service.calcularISR(_dbConnection, id_cliente);
+            try
+            {
+                SentrySdk.CaptureMessage("Post ISR Complete", SentryLevel.Info);
+                return _service.calcularISR(_dbConnection, id_cliente);
+            }
+            catch (Exception ex)
+            {
+                SentrySdk.CaptureException(ex);
+                SentrySdk.CaptureMessage("Error al calcular el ISR", SentryLevel.Error);
+                throw ex;
+            }
+            
         }
 
         [HttpPost("CalculoIVA")]
         public List<Calculo_Cliente> PostIVA(int id_cliente)
         {
-            return _service.calcularIVA(_dbConnection, id_cliente);
+            try
+            {
+                SentrySdk.CaptureMessage("Post IVA complete", SentryLevel.Info);
+                return _service.calcularIVA(_dbConnection, id_cliente);
+            }
+            catch (Exception ex)
+            {
+                SentrySdk.CaptureException(ex);
+                SentrySdk.CaptureMessage("Error al calcular el IVA", SentryLevel.Error);
+                throw ex;
+            }
+            
         }
 
         [HttpGet("CalculosClientes")]
         public List<Calculo_Cliente> GetCals()
         {
-            return _service.GetCalculosClientes(_dbConnection);
+            try
+            {
+                SentrySdk.CaptureMessage("Get Completo calculos de Clientes", SentryLevel.Info);
+                return _service.GetCalculosClientes(_dbConnection);
+            }
+            catch (Exception ex)
+            {
+                SentrySdk.CaptureException(ex);
+                SentrySdk.CaptureMessage("Error al obtener los calculos de Clientes", SentryLevel.Error);
+                throw ex;
+            }
+            
         }
     }
 }
